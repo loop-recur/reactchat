ddb8f10c72de0daee838da2c52a6aaa9e3caab2c
Joe Nelson <cred+github@begriffs.com>
2015-01-05 16:33:36 -0800

    Crude thing kind of works

---
+++ chat.js
@@ -0,0 +1,245 @@
+var Users = [];
+var Messages = [];
+
+var UsersList = React.createClass({
+  render: function(){
+    var renderUser = function(user){
+      return <li> { user} </li>
+    };
+    return (
+      <div class='users'>
+      <h3> Online Users </h3>
+      <ul>{ this.props.users.map(renderUser)} </ul>               
+      </div>
+    );
+  }
+});
+
+var Message = React.createClass({
+  render: function(){
+    return(
+      <div class="message">
+      <strong>{this.props.user}</strong> :
+        {this.props.text}
+      </div>
+    )
+  }
+});
+
+var MessageList = React.createClass({
+  render: function(){
+    var renderMessage = function(message){
+      return <Message user={message.user} text={message.text} />
+    }
+    return (
+      <div class='messages'>
+      <h2> Conversation: </h2>
+      { this.props.messages.map(renderMessage)}
+      </div>
+    );
+  }
+});
+
+var MessageForm = React.createClass({
+
+  getInitialState: function(){
+    return {text: ''};
+  },
+
+  handleSubmit : function(e){
+    e.preventDefault();
+    var message = {
+      user : this.props.user,
+      text : this.state.text
+    }
+    this.props.onMessageSubmit(message); 
+    this.setState({ text: '' });
+  },
+
+  changeHandler : function(e){
+    this.setState({ text : e.target.value });
+  },
+
+  render: function(){
+    return(
+      <div class='message_form'>
+      <h3>Write New Message</h3>
+      <form onSubmit={this.handleSubmit}>
+      <input onChange={this.changeHandler} value={this.state.text} />
+      </form>
+      </div>
+    );
+  }
+});
+
+var ChangeNameForm = React.createClass({
+  getInitialState: function(){
+    return {newName: ''};
+  },
+
+  onKey : function(e){
+    this.setState({ newName : e.target.value });
+  },
+
+  handleSubmit : function(e){
+    e.preventDefault();
+    var newName = this.state.newName;
+    this.props.onChangeName(newName);    
+    this.setState({ newName: '' });
+  },
+
+  render: function(){
+    return(
+      <div class='change_name_form'>
+      <h3> Change Name </h3>
+      <form onSubmit={this.handleSubmit}>
+      <input onChange={this.onKey} value={this.state.newName} />
+      </form>  
+      </div>
+    );
+  }
+});
+
+
+var ChatApp = React.createClass({
+
+  getInitialState: function(){
+
+    pubnub.subscribe({
+      channel: 'chat4:init',
+      message: this.initialize
+    });
+
+    pubnub.subscribe({
+      channel: 'chat4:send:message',
+      message: this.messageRecieve
+    });
+
+    pubnub.subscribe({
+      channel: 'chat4:send:join',
+      message: this.userJoined
+    });
+
+    pubnub.subscribe({
+      channel: 'chat4:send:left',
+      message: this.userLeft
+    });
+
+    pubnub.subscribe({
+      channel: 'chat4:change:name',
+      message: this.userChangedName
+    });
+
+    return {users: [], messages:[], text: ''};
+  },
+
+  initialize: function(data){
+    Users = data.users;
+    this.setState({ users: Users, user: data.name});
+  },
+
+  messageRecieve: function(message){
+    Messages.push(message);
+    this.setState({ messages : Messages });
+  },
+
+  userJoined: function(data){
+    Users.push(data.name);
+    Messages.push({
+      user: 'APLICATION BOT',
+      text : data.name +' Joined'
+    });
+    this.setState({ users : Users, messages: Messages});
+  },
+
+  userLeft: function(data){
+    var index = Users.indexOf(data.name);
+    Users.splice(index, 1);
+    Messages.push({
+      user: 'APLICATION BOT',
+      text : data.name +' Left'
+    });
+    this.setState({ users : Users, messages: Messages});
+  },
+
+  userChangedName : function(data){
+    var oldName = data.oldName;
+    var newName = data.newName;
+    Users.splice(Users.indexOf(oldName), 1, newName);
+    Messages.push({
+      user: 'APLICATION BOT',
+      text : 'Change Name : ' + oldName + ' ==> '+ newName
+    });
+    this.setState({ users : Users, messages: Messages});
+  },
+
+  handleMessageSubmit : function(message){
+    Messages.push(message);
+    this.setState({ messages : Messages });
+    pubnub.publish({
+      channel: 'chat4:send:message',
+      message: JSON.stringify(message)
+    });
+  },
+
+  handleChangeName : function(newName){
+    var that = this;
+    var oldName = this.state.user;
+    pubnub.publish({
+      channel: 'chat4:change:name',
+      message: JSON.stringify({name : newName})
+    });
+    var index = Users.indexOf(oldName);
+    Users.splice(index, 1, newName);
+    that.setState({users : Users});
+    // socket.emit('change:name', { name : newName}, function(result){
+    //   if(!result){
+    //     alert('There was an error changing your name');
+    //   }else{
+    //     var index = Users.indexOf(oldName);
+    //     Users.splice(index, 1, newName);
+    //     that.setState({users : Users});
+    //   }
+    // });
+  },
+
+  render : function(){
+    return (
+      <div>
+      <UsersList users={this.state.users} />
+      <MessageList messages={this.state.messages} />
+      <MessageForm onMessageSubmit={this.handleMessageSubmit} user={this.state.user} />
+      <ChangeNameForm onChangeName={this.handleChangeName} />
+      </div>
+    );
+  }
+});
+
+
+// var Message;
+// window.Chat = {
+//   Message: Message = React.createClass({
+//     render: function(){
+//       return(
+//         <div class="message">
+//         <strong>{this.props.user}</strong> :
+//           {this.props.text}
+//         </div>
+//       )
+//     }
+//   }),
+
+//   MessageList: React.createClass({
+//     render: function(){
+//       var renderMessage = function(message){
+//         return <Message user={message.user} text={message.text} />
+//       }
+//       return (
+//         <div class='messages'>
+//         <h2> Conversation: </h2>
+//         { this.props.messages.map(renderMessage) }
+//         </div>
+//       );
+//     }
+//   })
+// };
